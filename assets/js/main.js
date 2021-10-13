66c84a5b80df1b5073286850f011c6b63e2c2270
pizzi9523 <pizzitolagiuseppe@live.it>
2021-10-13 15:34:39 +0200

    Imposto uno stile diverso a seconda del valore i

--- assets/js/main.js
+++ assets/js/main.js
@@ -2,11 +2,29 @@
 const containerElement = document.querySelector(".container");
 
 
-for (let i = 0; i <= 100; i++) {
+for (let i = 1; i <= 100; i++) {
     //console.log(i);
     const number = document.createElement("p");
     number.append(i)
     containerElement.append(number)
+
+    // A seconda del valore dell'indice imposto uno stile differende per multipli di 3 e di 5 e/o entrambi
+    if (i % 3 == 0 && i % 5 == 0) {
+        number.style.backgroundColor = "#DE5471";
+        number.innerHTML = "fizzbuzz"
+    }
+    else if (i % 3 == 0) {
+        number.style.backgroundColor = "#61D2A4";
+        number.innerHTML = "fizz"
+    }
+    else if (i % 5 == 0) {
+        number.style.backgroundColor = "#F9D277";
+        number.innerHTML = "buzz"
+    }
+    else {
+        number.style.backgroundColor = "#3D88AE";
+    }
+
 }
-// A seconda del valore dell'indice imposto uno stile differende per multipli di 3 e di 5 e/o entrambi
+
 
