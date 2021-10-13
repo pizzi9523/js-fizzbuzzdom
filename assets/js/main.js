7c519770e2a9688d0891981ff4f474abfc0b6748
pizzi9523 <pizzitolagiuseppe@live.it>
2021-10-13 15:05:03 +0200

    Initial Commiti
    Struttura di base

---
+++ assets/js/main.js
@@ -0,0 +1,4 @@
+// Esegui un ciclo da 1 a 100 e per ogni iterazione aggiungi un elemento html nel container
+
+// A seconda del valore dell'indice imposto uno stile differende per multipli di 3 e di 5 e/o entrambi
+
