be76437c1d5db7340ada956bd5913a1c142a775a
pizzi9523 <pizzitolagiuseppe@live.it>
2021-10-13 15:16:41 +0200

    Creo i numeri e li aggiungo al file html

--- assets/js/main.js
+++ assets/js/main.js
@@ -1,4 +1,12 @@
 // Esegui un ciclo da 1 a 100 e per ogni iterazione aggiungi un elemento html nel container
+const containerElement = document.querySelector(".container");
 
+
+for (let i = 0; i <= 100; i++) {
+    //console.log(i);
+    const number = document.createElement("p");
+    number.append(i)
+    containerElement.append(number)
+}
 // A seconda del valore dell'indice imposto uno stile differende per multipli di 3 e di 5 e/o entrambi
 
